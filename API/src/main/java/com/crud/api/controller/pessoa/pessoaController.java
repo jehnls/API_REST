7a5c36a4c8ae30552a1156720922fec868535ace
Jeferson Nolasco <Jeferson Nolasco@sindeepresorg.local>
2020-01-31 16:40:33 -0300

    Incluir pessoa, finalizado com sucesso

--- API/src/main/java/com/crud/api/controller/pessoa/pessoaController.java
+++ API/src/main/java/com/crud/api/controller/pessoa/pessoaController.java
@@ -3,9 +3,14 @@ package com.crud.api.controller.pessoa;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.api.entidade.pessoa.Pessoa;
@@ -14,28 +19,30 @@ import com.crud.api.repository.PessoaRepository;
 @RestController
 @RequestMapping("/api/pessoa")
 public class pessoaController {
-	
+
 	@Autowired
 	PessoaRepository pessoaRepository;
-	
+
 	@GetMapping("/listar")
-	public List<Pessoa> listarPessoas(){
+	public List<Pessoa> listarPessoas() {
 		return pessoaRepository.findAll();
 	}
-	
-	@PostMapping("/save")
-	public void salvarPessoa (Pessoa pessoa) {
-		
-		pessoaRepository.save(pessoa);
-		
-	}
-	
-	//PUT
-	
-	//DELETE
-	
 
-	
-	
-	
+	@PostMapping("/cadastrar")
+	public void salvarPessoa(@RequestBody Pessoa pessoa) {
+		pessoaRepository.save(pessoa);
+	}
+
+	@PutMapping("/alterar")
+	public Pessoa alterarPessoa(@RequestBody Pessoa pessoa) {
+		pessoaRepository.save(pessoa);
+		return pessoa;
+	}
+
+	@DeleteMapping("/excluir")
+	public Pessoa excluirPessoa(@RequestBody Pessoa pessoa) {
+		pessoaRepository.delete(pessoa);
+		return pessoa;
+	}
+
 }
