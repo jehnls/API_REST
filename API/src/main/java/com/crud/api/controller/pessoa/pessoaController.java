10f096af13d42b1b97723227a28a7a1953ce5017
Jeferson Nolasco <Jeferson Nolasco@sindeepresorg.local>
2020-03-03 16:11:32 -0300

    Foi feita a inclusão do controlador CORS polity

--- API/src/main/java/com/crud/api/controller/pessoa/pessoaController.java
+++ API/src/main/java/com/crud/api/controller/pessoa/pessoaController.java
@@ -3,20 +3,20 @@ package com.crud.api.controller.pessoa;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.api.entidade.pessoa.Pessoa;
 import com.crud.api.repository.PessoaRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/pessoa")
 public class pessoaController {
 
